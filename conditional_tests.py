51909169579495b0fde5ac334658f6164298e864
kaitlynnbeston1 <113719685+kaitlynnbeston1@users.noreply.github.com>
2022-11-30 10:47:29 -0600

    Here are the first conditional tests I wrote.

---
+++ conditional_tests.py
@@ -0,0 +1,29 @@
+print("IS 12*2 equal to 24? I predict true.")
+print(12 * 2 == 24)
+print("Does 35+2 not equal 37? I predict false.")
+print(35 + 2 != 37) 
+print("Is 12/2 < 10? I predict true.")
+print(12/2 < 10)
+print("IS 54 % 5 > 32? I predict false.")
+print(54 % 5 > 32)
+randomNumber = 6392 + 3607 
+print("Is randomNumber <= 10,000? I predict true.")
+print(randomNumber <= 10000) 
+randomNumber2 = 3 * 100 
+print("Is randomNumber2 <= 300? I predict true.")
+print(randomNumber2 <= 300)
+randomNumber3 = 11*7
+print("Is randomNumber3 >= 123? I predict false.")
+print(randomNumber3 >= 123)
+randomNumber4 = 374
+print("Does randomNumber4 == 666? I predict false.")
+print(randomNumber4 == 666)
+myAge = 12+5
+votingAge = 9 * 2
+print("Am I able to vote? Does myAge = votingAge? I predict false")
+print(myAge >= votingAge)
+seniorDiscount = 5 * 13
+JeffAge = 9 ** 2
+print("At store X, you can recieve a senior discount if you are 65 or older.")
+
+print("Is Jeff able to recieve a senior discount? I predict true.")
